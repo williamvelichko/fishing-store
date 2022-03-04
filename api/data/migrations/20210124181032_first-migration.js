f495931ec410297c9f6de24e241b7a766151c2d2
williamvelichko <williamvelichko2003@gmail.com>
2022-03-04 12:11:37 -0800

    save

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,22 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
+    .createTable("users", (users) => {
+      users.increments("user_id");
+      users.string("username", 200).notNullable();
+      users.string("password", 200).notNullable();
+      users.timestamps(false, true);
     })
-}
+    .createTable("fishing-items", (tbl) => {
+      tbl.increments("item_id");
+      tbl.string("name", 200).notNullable();
+      tbl.string("description", 200).notNullable();
+      tbl.decimal("price").notNullable();
+      tbl.integer("rating").notNullable();
+    });
+};
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-}
+  await knex.schema
+    .dropTableIfExists("users")
+    .dropTableIfExists("fishing-items");
+};
